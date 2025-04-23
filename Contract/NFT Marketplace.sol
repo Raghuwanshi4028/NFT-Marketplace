0037c99a4d91c3d2d1ae7819cbe988592730c2be
Viraj Raghuwanshi <virajraghuwanshi4028@gmail.com>
2025-04-23 15:54:07 +0530

    Update NFT Marketplace.sol

--- Contract/NFT Marketplace.sol
+++ Contract/NFT Marketplace.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+progma solidity ^0.8.20;
+ 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract NFTMarketplace {
+    struct Listing {
+        address seller;
+        address nftAddress;
+        uint256 tokenId;
+        uint256 price;
+    }
+
+    mapping(uint256 => Listing) public listings;
+    uint256 public listingCounter;
+
+    event Listed(uint256 listingId, address seller, address nftAddress, uint256 tokenId, uint256 price);
+    event Purchased(uint256 listingId, address buyer);
+
+    function listNFT(address nftAddress, uint256 tokenId, uint256 price) external {
+        require(price > 0, "Price must be greater than zero");
+
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), tokenId);
+
+        listings[listingCounter] = Listing(msg.sender, nftAddress, tokenId, price);
+        emit Listed(listingCounter, msg.sender, nftAddress, tokenId, price);
+        listingCounter++;
+    }
+
+    function buyNFT(uint256 listingId) external payable {
+        Listing memory item = listings[listingId];
+        require(msg.value == item.price, "Incorrect ETH sent");
+
+        payable(item.seller).transfer(msg.value);
+        IERC721(item.nftAddress).transferFrom(address(this), msg.sender, item.tokenId);
+
+        delete listings[listingId];
+        emit Purchased(listingId, msg.sender);
+    }
+}
 
